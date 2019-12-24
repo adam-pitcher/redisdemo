85be430a5674cd34cfd4888f0e4ce1b6ad9184de
Adam <apitcher@apitcher.net>
2019-12-24 11:09:51 -0500

    Added simple rest endpoints for Demo purposes

--- main.go
+++ main.go
@@ -3,15 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+	"flag"
 	"github.com/redisdemo/redisgo"
 	"github.com/redisdemo/goredis"
+	"github.com/redisdemo/redisrest"
 	
 )
 
 func main(){
+	redisgoFlag := flag.Bool("redisgo",false,"Run redis-go demo")
+	goredisFlag := flag.Bool("goredis",false,"Run go-redis demo")
+	redisRest := flag.Bool("redisRest",false,"Start a server which interacts with Redis via a REST Api")
+
+	flag.Parse()
+
 	fmt.Println("Go is working")
 	log.Println("Log is working")	
-	redisgo.Demo()
-	goredis.Demo()
 
+	if *redisgoFlag{
+		redisgo.Demo()
+	}
+
+	if *goredisFlag{
+		goredis.Demo()
+	}
+
+	if *redisRest{
+		redisrest.InitializeServer()
+
+	}
 }
+
